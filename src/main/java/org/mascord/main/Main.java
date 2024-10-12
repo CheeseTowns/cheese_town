371693e2774d9c8bd883cde6fa7d1be6b274415d
FIRO <appturbo102@gmail.com>
2024-10-12 14:22:19 +0900

    feat: main.java에 PlayerJoinListener 이벤트 추가

--- src/main/java/org/mascord/main/Main.java
+++ src/main/java/org/mascord/main/Main.java
@@ -2,6 +2,7 @@ package org.mascord.main;
 
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mascord.main.listeners.PlayerJoinListener;
 import org.mascord.main.logger.MascordLogger;
 
 public final class Main extends JavaPlugin implements Listener {
@@ -12,6 +13,9 @@ public final class Main extends JavaPlugin implements Listener {
         MascordLogger.init(getLogger());
 
         MascordLogger.info("플러그인이 활성화되었습니다.");
+
+        // 이벤트 등록
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
     }
 
     @Override
