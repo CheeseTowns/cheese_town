b063904fd6e35a3fbe3ece6ed25070764fd1eb0e
FIRO <appturbo102@gmail.com>
2024-10-13 21:28:06 +0900

    refactor: Main.java MongoDB 싱글톤 인스턴스로 불러오게 수정

--- src/main/java/org/mascord/main/Main.java
+++ src/main/java/org/mascord/main/Main.java
@@ -6,31 +6,31 @@ import org.mascord.main.commands.Whisper;
 import org.mascord.main.listeners.PlayerJoinListener;
 import org.mascord.main.logger.MascordLogger;
 import org.mascord.main.utils.ScoreBoard;
-import org.mascord.main.utils.mongodb;
+import org.mascord.main.utils.MongoDB;
 
 public final class Main extends JavaPlugin implements Listener {
-    private mongodb mongoManager;
 
     @Override
     public void onEnable() {
-        mongoManager = new mongodb();
+        // MongoDB 싱글톤 인스턴스 가져오기
+        MongoDB mongoManager = MongoDB.getInstance();
         mongoManager.connect("mongodb+srv://mascord:DefaultPassword@mascord.iob4z.mongodb.net/", "mydb");
 
         // 이벤트 등록
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(mongoManager), this);
+        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
 
-        // Event
-        new ScoreBoard(this, mongoManager);
-        // Commands
+        // ScoreBoard 생성 및 초기화
+        new ScoreBoard(this);
+
+        // 명령어 등록
         getCommand("귓").setExecutor(new Whisper());
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
-        if (mongoManager != null) {
-            mongoManager.disconnect();
-        }
+        // MongoDB 연결 해제
+        MongoDB mongoManager = MongoDB.getInstance();
+        mongoManager.disconnect();
         MascordLogger.info("플러그인 비활성화 및 MongoDB 연결 해제 완료");
     }
 }
