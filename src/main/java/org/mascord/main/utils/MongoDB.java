0abc5f5c76247630f29be21a77532cc4e7b7d0a0
FIRO <appturbo102@gmail.com>
2024-10-13 21:40:58 +0900

    feat: .gitignore에 .gradle 폴더 추가

--- src/main/java/org/mascord/main/utils/MongoDB.java
+++ src/main/java/org/mascord/main/utils/MongoDB.java
@@ -2,10 +2,7 @@ package org.mascord.main.utils;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-
-import org.bson.Document;
 
 public class MongoDB {
     private static MongoDB instance; // 싱글톤 인스턴스
