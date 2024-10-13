857f1ea9ab369e292e57e936097982c5a12127c8
FIRO <appturbo102@gmail.com>
2024-10-13 17:35:09 +0900

    fix: MongoDB가 모니터가 안정화 될때 까지 비활성화

--- src/main/java/org/mascord/main/Main.java
+++ src/main/java/org/mascord/main/Main.java
@@ -2,6 +2,7 @@ package org.mascord.main;
 
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mascord.main.commands.Whisper;
 import org.mascord.main.listeners.PlayerJoinListener;
 import org.mascord.main.logger.MascordLogger;
 import org.mascord.main.utils.ScoreBoard;
@@ -12,12 +13,12 @@ public final class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        mongoManager = new mongodb();
-        mongoManager.connect("mongodb+srv://mascord:DefaultPassword@mascord.iob4z.mongodb.net/", "mascord");
+//        mongoManager = new mongodb();
+//        mongoManager.connect("mongodb+srv://mascord:DefaultPassword@mascord.iob4z.mongodb.net/", "mascord");
         // MascordLogger 인스턴스 초기화
-        MascordLogger.init(getLogger());
-
-        MascordLogger.info("플러그인 활성화 및 MongoDB 연결 완료");
+//        MascordLogger.init(getLogger());
+//
+//        MascordLogger.info("플러그인 활성화 및 MongoDB 연결 완료");
 
         // 이벤트 등록
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
